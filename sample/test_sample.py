24176705672b61a946d48c4f929fbb5d08cbddcf
Yu Osada <osada@shisui.tokyo>
2026-05-31 16:29:12 +0900

    chore: move sample code to sample/ directory

---
+++ sample/test_sample.py
@@ -0,0 +1,14 @@
+score = 85
+
+if score >= 90:
+    print("優秀")
+elif score >= 70:
+    print("合格")
+else:
+    print("不合格")
+
+total = 0
+for i in range(1, 6):
+    total = total + i
+
+print(total)
